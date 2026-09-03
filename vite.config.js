0ccebcea0b2b001791f374b367f00eb62e1a29d0
Fabol <fabol@example.com>
2026-09-03 13:45:55 +0300

    ci: add GitHub Actions workflow for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
-  base: './',
-  build: { outDir: 'dist' }
-})
+  base: '/botamin/',
+  build: {
+    outDir: 'dist'
+  }
+});
